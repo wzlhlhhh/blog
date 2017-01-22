ad5bfc921dc92822fd7d9b9a8a853af85c55e52c
wzlhlhhh <411491847@qq.com>
2017-01-22 11:46:12 +0800

    version 1.0
    GerEnBoKe
    s Please enter the commit message for your changes. Lines starting

---
+++ index.php
@@ -0,0 +1,104 @@
+<html>
+	<head>
+		<title>个人首页 ｜ 博客</title>
+		<style type="text/css">
+			html {height: 98%; }
+			body {height: 98%;text-align:center; }
+			h2{line-height: 1; font-size: 30px; font-family: "微软雅黑"; display: inline; }
+			h4{line-height: 0.78; font-size: 15px; font-family: gadget; height: 18px; display: inline;}
+			h5{font-size: 24px; font-family: "微软雅黑"; }
+		</style>
+		
+	</head>
+	<body style="width: 1400px; background: #eff7ff;">
+	<script type="text/javascript">
+			var w_height = document.body.scrollHeight;;
+			//alert(document.body.clientHeight);
+			if(w_height > 740)
+			{
+				<?php $i = 1;?>
+			}
+			else
+			{
+				<?php $i = 2;?>
+			}
+		</script>
+		<div id="" style="margin:0 auto;width: 1330px; height: 100%; border:1px solid #bfd1eb;background:#f3faff;">
+			<div id="" style="z-index:1000;width:200px;position: relative; left: 20px; top:20px;  border:1px solid #96c2f1;background:aliceblue; height: 95%; width: 350px;">
+			<!--
+            	作者：411491847@qq.com
+            	时间：2017-01-16
+            	描述：左边一块，是目录、头像等
+         	-->
+         		<div id="" style="position: relative;top: 5%; height: 250px;">
+         			<!--
+                     	作者：411491847@qq.com
+                     	时间：2017-01-16
+                     	描述：头像
+                     -->
+                     <img src="image/tx.png"/ style="height: 150px; width: 150px; border-radius: 75px; ">
+                     <div id="" style="height: 40px; font-size: 35px; font-weight: bold;">
+                     	YR’s Blog</br>
+                     	-----------------
+                     </div>
+                     <!--
+                     	作者：411491847@qq.com
+                     	时间：2017-01-16
+                     	描述：文章类别
+                     -->
+                     
+         		</div>
+         		<div id="" style="position: relative; top: 5%; border: 2px;">
+                     <div id="" style="position: relative; top: -5px; font-family: kartika; font-size: 25px;">
+                     	文章类别
+                     </div>
+                     	<ul style="float: left; font-size: 20px; line-height: 1.5em;">
+                     		<li>PHP</li>
+                     		<li>PHP</li>
+                     		<li>PHP</li>
+                     		<li>PHP</li>
+                     		<li>PHP</li>
+                     	</ul>
+                </div>
+			</div>
+			<div id="" style="position: relative; left: 400px; top: -94%; height: 97%; width: 900px; border:1px solid #96c2f1;">
+				<!--
+	            	作者：411491847@qq.com
+	            	时间：2017-01-16
+	            	描述：右边一块，近期预览
+	            -->
+	            <div id="" style="font-family: simhei; font-size: 40px; font-weight: bold; position:relative;top: 1%;">
+	            	纹赃区
+	            </div>
+	            <div align="left" id="" style="position: relative;top: 2%; left: 5%;">
+                    	<div id="" style="width: 800px;">
+                    		<h2>题目<h4>(时间)</h4></h2>
+                    		<h5 style="position: relative; top: -30px;">正文正文正文正文正文正文正文正文正文正文正文正文正文正文正
+                    			文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正
+                    			文正文正文正文正文正文正文正文正文正文正文正文正文正文正文
+                    			正文正文正文正文正文正文正文正文正文正文正文正文正文</h5>
+                    	</div>
+                    	<div id="" style="width: 800px; position: relative; top: -40px;">
+                    		<h2>题目</h2><h4>(时间)</h4>
+                    		<h5 style="position: relative; top: -30px;">正文正文正文正文正文正文正文正文正文正文正文正文正文正文正
+                    			文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正
+                    			文正文正文正文正文正文正文正文正文正文正文正文正文正文正文
+                    			正文正文正文正文正文正文正文正文正文正文正文正文正文</h5>
+                    	</div>
+                    	<?php if($i == 1){?><div id="" style="width: 800px; position: relative; top: -80px;">
+                    		<h2>题目</h2><h4>(时间)</h4>
+                    		<h5 style="position: relative; top: -30px;">正文正文正文正文正文正文正文正文正文正文正文正文正文正文正
+                    			文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正
+                    			文正文正文正文正文正文正文正文正文正文正文正文正文正文正文
+                    			正文正文正文正文正文正文正文正文正文正文正文正文正文</h5><?php }?>
+                    			<div align="center" id="" style="position: relative; left: -50px; top: -30px;" >
+                    		首页  1  2  3  4  尾页  
+                    	</div>
+                    	</div>
+                    	
+	            </div>
+			</div>
+		</div>
+	</body>
+	
+</html>
